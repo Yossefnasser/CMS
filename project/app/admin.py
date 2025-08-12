a895c276f0887b82b7d4a02587d5875a479603f4
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-08-12 07:45:31 +0300

    templete

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Patient, Clinic, DoctorSchedule, Appointment, Invoice
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Patient)
+admin.site.register(Clinic)
+admin.site.register(DoctorSchedule)
+admin.site.register(Appointment)
+admin.site.register(Invoice)
