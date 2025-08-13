6ff64425ea98739f1302f9674249ef2cf4b4f9c9
Ahmed Mohamed <markov916966@gmail.com>
2025-08-13 18:45:38 +0300

    fix modal

--- project/app/com/new_patient.py
+++ project/app/com/new_patient.py
@@ -3,17 +3,30 @@ from app.models import Patient
 
 def add_patient(request):
     if request.method == 'POST':
+        full_name      =   request.POST.get('full_name')
+        phone_number   =   request.POST.get('phone')
+        notes          =   request.POST.get('notes', '')
+
+        patient_obj    = Patient.objects.filter(phone_number=phone_number).first()
+        if patient_obj:
+            return JsonResponse({
+                'success': False,
+                'error': 'المريض مسجل بالفعل',
+                
+            })
+
         try:
             patient = Patient.objects.create(
-                full_name=request.POST.get('full_name'),
-                phone_number=request.POST.get('phone'),
-                notes=request.POST.get('notes', '')
+                full_name      =   full_name,
+                phone_number   =   phone_number,
+                notes          =   notes
             )
             
             return JsonResponse({
                 'success': True,
                 'patient_id': patient.id,
-                'patient_name': patient.full_name
+                'patient_name': patient.full_name,
+                'message': 'تم إضافة المريض بنجاح'
             })
             
         except Exception as e:
