b7d7090019777de596562727df03e0dd71e4f539
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-09-15 16:01:49 +0300

    up

--- project/project/settings.py
+++ project/project/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+import os
+
+# Database configuration
+if os.environ.get('DATABASE_URL'):
+    # Production (Railway) - PostgreSQL
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    # Local development - SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
