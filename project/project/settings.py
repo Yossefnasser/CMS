f5436fbf83a86c27b80612ff6bf5043fb89cf4f1
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-09-21 11:35:40 +0300

    another try

--- project/project/settings.py
+++ project/project/settings.py
@@ -84,7 +84,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
+        'NAME': 'railway',
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
