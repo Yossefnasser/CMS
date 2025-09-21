149dc9e3455242a9cce75d7fc852604518ea60ca
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-09-21 11:54:17 +0300

    commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -83,13 +83,14 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'NAME': env('PGDATABASE', default='railway'),
+        'USER': env('PGUSER', default='postgres'),
+        'PASSWORD': env('PGPASSWORD', default=''),
+        'HOST': env('PGHOST', default='localhost'),
+        'PORT': env('PGPORT', default='5432'),
     }
 }
+
 
 
 # Password validation
