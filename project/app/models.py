7dac47b7e5a18c9af18c983e2490a1e14957e077
Yossefnasser <yusef_20230697@fci.helwan.edu.eg>
2025-10-15 18:22:55 +0300

    commit

--- project/app/models.py
+++ project/app/models.py
@@ -210,10 +210,7 @@ class Appointment(BaseModel):
     doctor   = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     clinic   = models.ForeignKey(Clinic, on_delete=models.CASCADE)
     status   = models.ForeignKey(Status, on_delete=models.CASCADE)
-    service_type    = models.CharField(max_length=20, choices=[
-        ('consultation', 'Consultation'),
-        ('examination', 'Examination'),
-    ], default='consultation')
+    service_type    = models.CharField(max_length=20)
     service_price   = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     date     = models.DateField()
     time     = models.TimeField()
@@ -236,8 +233,8 @@ class Appointment(BaseModel):
             'status'    : self.status.name,
             'date'      : self.date,
             'time'      : self.time.strftime('%H:%M'),
-            'serivce_type'      : self.service_type,
-            'service_price'     : self.service_type,
+            'service_type'      : self.service_type,
+            'service_price'     : self.service_price,
             'notes'     : self.notes,
         }
 
